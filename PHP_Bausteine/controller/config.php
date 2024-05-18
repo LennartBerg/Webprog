6142d74be93db7f2f8d40417b8826627110fed33
JonasTheRipper <129778680+JonasTheRipper@users.noreply.github.com>
2024-05-18 09:06:33 +0200

    Einbettung der Datenbank, Button in footer um verbindung der Datenbank zu chenken, kann bei Abgabe gelöscht werden

---
+++ PHP_Bausteine/controller/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Hier die Einbindung der Datenbank üder ddevcontainer `db`, wird nicht über xampp funktionieren
+ */
+$databaseHost = 'db'; //
+$databaseUsername = 'db';
+$databasePassword = 'db';
+$databaseName = 'db';
+
+$conn = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+echo "Erfolgreich verbunden!";
+?>
