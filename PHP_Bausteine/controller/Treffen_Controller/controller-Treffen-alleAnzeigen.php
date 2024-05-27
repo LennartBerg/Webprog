41b07e1f1b2d67c1931d0decdb1989f193ce4969
JonasTheRipper <129778680+JonasTheRipper@users.noreply.github.com>
2024-05-27 17:20:05 +0200

    ADD controller

---
+++ PHP_Bausteine/controller/Treffen_Controller/controller-Treffen-alleAnzeigen.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if (!isset($abs_path)) {
+    require_once "../../../path.php";
+}
+
+require_once $abs_path . "/PHP_Bausteine/model/TreffenModel/TreffenListe.php";
+require_once $abs_path . "/PHP_Bausteine/model/TreffenModel/Treffen.php";
+
+
+try {
+    $TreffenListe = TreffenListe::getInstance();
+    $TreffenArray = $TreffenListe->getAllTreffen();
+} catch (InternerFehlerException $exc) {
+    // Behandlung von potentiellen Fehlern der Geschaeftslogik
+    $_SESSION["message"] = "internal_error";
+    header("Location: index.php");
+    exit;
+}
--- PHP_Bausteine/controller/Treffen_Controller/controller-Treffen-anzeigen.php
+++ PHP_Bausteine/controller/Treffen_Controller/controller-Treffen-anzeigen.php
@@ -1,1 +1,36 @@
 <?php
+
+session_start();
+if (!isset($abs_path)) {
+    require_once "../../../path.php";
+}
+
+require_once $abs_path . "/PHP_Bausteine/model/TreffenModel/TreffenListe.php";
+require_once $abs_path . "/PHP_Bausteine/model/TreffenModel/Treffen.php";
+
+// Ueberpruefung der Parameter
+//TODO: Workaround überlegen wie wir den Parameter für ID bekommen,lol
+if (!isset($_GET["id"]) || !is_numeric($_GET["id"])) {
+    $_SESSION["message"] = "invalid_Treffen_id";
+    header("Location: index.php");
+    exit;
+}
+
+try {
+
+    $id = intval($_GET["id"]);
+
+
+    $treffenListe = TreffenListe::getInstance();
+    $treffen = $treffenListe->getTreffen($id);
+} catch (FehlenderNutzerException $exc) {
+
+    $_SESSION["message"] = "invalid_nutzer_id";
+    header("Location: index.php");
+    exit;
+} catch (InternerFehlerException $exc) {
+
+    $_SESSION["message"] = "internal_error";
+    header("Location: index.php");
+    exit;
+}
