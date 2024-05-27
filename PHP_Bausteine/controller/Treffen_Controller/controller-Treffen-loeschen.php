447014eb1fc1cab0845bb4cca1fd612910db8190
JonasTheRipper <129778680+JonasTheRipper@users.noreply.github.com>
2024-05-27 17:29:41 +0200

    ADD controller-Treffen-loeschen.php

--- PHP_Bausteine/controller/Treffen_Controller/controller-Treffen-loeschen.php
+++ PHP_Bausteine/controller/Treffen_Controller/controller-Treffen-loeschen.php
@@ -1,1 +1,27 @@
 <?php
+if (!isset($abs_path)) {
+    require_once "../../../path.php";
+}
+require_once $abs_path . "/PHP_Bausteine/model/TreffenModel/TreffenListe.php";
+require_once $abs_path . "/PHP_Bausteine/model/TreffenModel/Treffen.php";
+
+$TreffenID = isset($_POST[htmlspecialchars("TreffenID")]) ? $_POST[htmlspecialchars("TreffenID")] : "";
+
+if(!isset($_POST[$TreffenID])){
+    $_SESSION["message"] = "missing_data";
+    header("Location: ../../../Treffen.php");
+    exit;
+}
+try{
+    $tmp = TreffenListe::getInstance() ->  loescheTreffen($TreffenID);
+    exit;
+} catch (InternerFehlerInTreffenDatenbankException $exc){
+    $_SESSION["message"] = "internal_error";
+    header("Location: /index.php");
+    exit;
+}
+
+unset($_POST["TreffenID"]);
+
+header("Location: ../../../Rezepte.php");
+?>
