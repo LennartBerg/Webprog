7f5f30fbf659dcde63365c99fa257dfc80cc6a30
JonasTheRipper <129778680+JonasTheRipper@users.noreply.github.com>
2024-05-29 16:59:11 +0200

    TreffenListePDOSQLite.php erste implementierungen

--- index.php
+++ index.php
@@ -14,8 +14,18 @@ $treffen = $treffenController->getAllTreffen();
         <div class="main">
             <h1 class="centerPagename"> Home</h1>
             <br><br>
-            <h2 class="centerPagename">Neue Rezepte</h2>
+            <h2 class="centerPagename">Neue Treffen</h2>
             <div class="Objektliste">
+                <?php foreach ($treffen as $treffen): ?>
+                    <div class="Objekt">
+                        <img src="Pictures/Tofu.JPG" alt="Karte"">
+                        <div class="inneresObjekt">
+                            <h3><?php echo $treffen->getTitel(); ?></h3>
+                            <h5><?php echo $treffen->getErsteller(); ?></h5>
+                            <p class="entryText"><?php echo $treffen->getBeschreibung(); ?></p>
+                            <button class="EntryButton" type="submit"><b>Treffen beitreten</b></button>
+                        </div>
+                    </div>
                 <div class="Objekt">
                     <img src="Pictures/Tofu.JPG" alt="Karte"">
                     <div class="inneresObjekt">
