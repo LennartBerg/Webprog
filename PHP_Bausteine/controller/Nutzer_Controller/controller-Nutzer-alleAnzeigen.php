9708ebcd01327b92eee652556b5a4a74151fa3e3
JonasTheRipper <129778680+JonasTheRipper@users.noreply.github.com>
2024-05-29 13:09:26 +0200

    TreffenListePDOSQLite.php erste implementierungen

--- PHP_Bausteine/controller/Nutzer_Controller/controller-Nutzer-alleAnzeigen.php
+++ PHP_Bausteine/controller/Nutzer_Controller/controller-Nutzer-alleAnzeigen.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($abs_path)) {
-    require_once "../../../path.php";
+    require_once "./../../path.php";
 }
 
 require_once $abs_path . "/PHP_Bausteine/model/NutzerModel/NutzerListe.php";
