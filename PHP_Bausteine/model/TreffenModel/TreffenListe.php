3e476ae92868b9a6bea2c15f04d3958c06161eac
JonasTheRipper <129778680+JonasTheRipper@users.noreply.github.com>
2024-05-29 13:43:08 +0200

    TreffenListePDOSQLite.php erste implementierungen

--- PHP_Bausteine/model/TreffenModel/TreffenListe.php
+++ PHP_Bausteine/model/TreffenModel/TreffenListe.php
@@ -4,18 +4,11 @@ require_once dirname(__FILE__) . "/TreffenListePDOSQLite.php";
 
 class TreffenListe
 {
-    private static $instance = null;
-    private $treffens = array()
-    ;
     public static function getInstance()
     {
         if (self::$instance == null) {
             self::$instance = TreffenListePDOSQLite::getInstance();
         }
         return self::$instance;
-    }
-    public function getAllTreffen()
-    {
-        return $this->treffens;
     }
 }
