d5bfda34df20957bd03ed13b6a5f7651dd7f3446
Lennart Berg <lennart.berg@uni-oldenburg.de>
2024-05-30 09:08:42 +0200

    Paths

--- nutzerNeu.php
+++ nutzerNeu.php
@@ -4,4 +4,4 @@ require_once './vendor/autoload.php';
 $nutzerController = new \PHP_Bausteine\controller\NutzerController;
 $nutzerController->nutzerNeu();
 
-//@Markus und @Dibo, könnt ihr uns beim nächsten Tutorium vielleicht sagen weswegen keine Einträge in die Datenbank geschrieben werden?
+//@Markus und @Dibow, könnt ihr uns beim nächsten Tutorium vielleicht sagen weswegen keine Einträge in die Datenbank geschrieben werden?
